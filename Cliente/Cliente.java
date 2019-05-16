27f33785529432cedaadd257efea72212d458a15
vichinzi <44653674+vichinzi@users.noreply.github.com>
2019-05-16 18:40:03 -0400

    Cliente comentado, no estaba tan desordenado :o

--- Cliente/Cliente.java
+++ Cliente/Cliente.java
@@ -11,23 +11,34 @@ public class Cliente {
         //Host del servidor
         final String HOST;    //"127.0.0.1"
         System.out.println("Ingrese IP para la conexion");
-        Scanner scn = new Scanner(System.in);
+        
+        Scanner scn = new Scanner(System.in); //despues se rehusa para el menu
         HOST = scn.nextLine();
+
 
         //Puerto del servidor
         final int PUERTO = 11111;
 
         try {
 			
-            //Creo el socket para conectarme con el cliente
-            Socket sc = new Socket(HOST, PUERTO); //(1)peticion conexión
+			
+			
+            //Creo el socket para conectar con cliente
+            Socket sc = new Socket(HOST, PUERTO); //(1) peticion conexión
                         
-
-            boolean flag = true;
-            String mensaje;
-            String ls;
+                        
+            String mensaje;  //respuesta de menu, se envia a Server
+            
+            
+            //variables ocupadas en ls
+            String ls;  
             Boolean lsss;
-
+            
+            //variables usadas para put y get
+            String palabra;
+            String[] palabras 
+            
+			boolean flag = true;
             while(flag){
 
                 //Menu
@@ -42,8 +53,12 @@ public class Cliente {
                 System.out.println("-----------------------------------");
                 System.out.println("+Ingrese opcion:");
                 System.out.println(" ");
+                
+                
                 mensaje = scn.nextLine();
+                
                 DataOutputStream out = new DataOutputStream(sc.getOutputStream());
+                
                 out.writeUTF(mensaje); //le manda el mensaje al servidor por el socket sc ----> (3) ida al server
 
 
@@ -52,10 +67,11 @@ public class Cliente {
                 }
 
                 if (mensaje.equals("ls")){
+					
                     DataInputStream in = new DataInputStream(sc.getInputStream());
                     lsss = true;
                     while(lsss){
-                        ls = in.readUTF();  //(-><-) lee cada path enviado por el server
+                        ls = in.readUTF();  //(-><-) lee cada path enviado por el server hasta un mensaje Finisher
                         
                         if (ls.equals("Finisher")){
                             lsss=false;
@@ -64,13 +80,14 @@ public class Cliente {
                            System.out.println(ls); 
                         }
                     }
-                    mensaje = in.readUTF();
+                    
+                    mensaje = in.readUTF(); //despues borrar esto **
                 }
 
                 if (mensaje.split(" ")[0].equals("get")) {
-                    //InputStream in = sc.getInputStream();
-                    String[] palabras = mensaje.split(" ");
-                    String palabra = palabras[1].replaceAll("\\s",""); //ble.txt por ejemplo
+                    
+                    palabras = mensaje.split(" "); 
+                    palabra = palabras[1].replaceAll("\\s",""); 
                     try {
                         new Cliente().recivir(sc, palabra);
                     } catch (Exception ex) {
@@ -81,7 +98,7 @@ public class Cliente {
                 if (mensaje.split(" ")[0].equals("put")) {
                     DataInputStream in = new DataInputStream(sc.getInputStream());
 
-                    String palabra = in.readUTF(); //lee la palabra que le manda el if de put en server
+                    palabra = in.readUTF(); //lee la palabra que le manda el if de put en server
                     //OutputStream outs = sc.getOutputStream();
 
                     try {
@@ -93,6 +110,8 @@ public class Cliente {
 
 
                 else if (mensaje.split(" ")[0].equals("delete")){
+					
+					//se envia a Server para que elimine el archivo
                     DataInputStream in = new DataInputStream(sc.getInputStream());
                     System.out.println(in.readUTF());
                 }
@@ -108,37 +127,45 @@ public class Cliente {
     }
 
 
-    public void enviar(String palabra, Socket socket) throws Exception {
+    public void enviar(String palabra, Socket socket) throws Exception {  //funcion usada por el put
 
-        String concatenacion = System.getProperty("user.dir") + "/" + palabra;
-        System.out.println(concatenacion);
+        String concatenacion = System.getProperty("user.dir") + "/" + palabra;  //directorio de archivo a enviar
+        
+        //System.out.println(concatenacion);
+        
         File myFile = new File(concatenacion);
-        int largo = (int) myFile.length(); //funciona
-        String largito = Integer.toString(largo);
+        int largo = (int) myFile.length(); //largo de archivo
+        String largito = Integer.toString(largo); //same pero en string
 
 
         DataOutputStream osss = new DataOutputStream(socket.getOutputStream());
-        osss.writeUTF(largito);
+        
+        osss.writeUTF(largito); //se manda a server el largo del archivo
+        
         DataInputStream issss = new DataInputStream(socket.getInputStream());
         issss.readUTF();
+        
+        
         if(issss.equals("gracias")){
-        	System.out.println("llego el gracias");
+        	System.out.println("llego el gracias"); //largo recibido por server
         }
 
-        byte[] mybytearray = new byte[largo];
-        FileInputStream fis = new FileInputStream(concatenacion);
+        byte[] mybytearray = new byte[largo]; //este arreglo contendra el archivo para ser enviado
+        
+		//manejadores de archivos para pasarlo a bytes
+        FileInputStream fis = new FileInputStream(concatenacion);  
         BufferedInputStream bis = new BufferedInputStream(fis);
+        
+        //se usara para traspaso de bytes via socket
         BufferedOutputStream outt = new BufferedOutputStream(socket.getOutputStream());
 
         int current;
-        current = bis.read(mybytearray);
-        outt.write(mybytearray,0, current);
-
-
+        current = bis.read(mybytearray); //se pasa al arrego que se mencionó antes
         
-        //bis.read(mybytearray, 0, mybytearray.length);
+        //se manda archivo por socket
+        outt.write(mybytearray,0, current);
         System.out.println("Archivo enviado");
-        //os.write(mybytearray, 0, mybytearray.length);
+        
         outt.flush();
         fis.close();
         bis.close();
@@ -154,20 +181,25 @@ public class Cliente {
         String nombre = name;
 
         DataInputStream isss = new DataInputStream(socket.getInputStream());
-        largo = isss.readUTF();
-        largito = Integer.parseInt(largo);
+        largo = isss.readUTF(); //se lee el largo del archivo enviado
+        largito = Integer.parseInt(largo); //se castea a entero
 
         DataOutputStream osss = new DataOutputStream(socket.getOutputStream());
-        osss.writeUTF("gracias");
+        osss.writeUTF("gracias");  //comunicacion con cliente, listo para la recepción del archivo
 
-        InputStream iss = socket.getInputStream();
-        byte[] mybytearray = new byte[largito];
-        FileOutputStream fos = new FileOutputStream(nombre);
+        InputStream iss = socket.getInputStream(); //se crea la conexion con server
+        
+        byte[] mybytearray = new byte[largito]; //arreglo de bytes con largo recibido
+        
+      
+        current = iss.read(mybytearray);  //se traspasa a bytes la informacion recibida
+        
+        //manejadores de archivo nuevo
+        FileOutputStream fos = new FileOutputStream(nombre);  
         BufferedOutputStream bos = new BufferedOutputStream(fos);
 
-        current = iss.read(mybytearray); //nunca esta leyendo el -1 porqueeee!!!
-
-        bos.write(mybytearray, 0, current);
+		//se crea el archivo recibido, desde los bytes
+        bos.write(mybytearray, 0, current); 
 
         //bos.write(mybytearray, 0, bytesRead);
         //bos.flush();
