22c96e900078d8fd00123af08f1223a50a7a7d42
Ekfrancisco <38672375+Ekfrancisco@users.noreply.github.com>
2019-05-17 17:14:05 -0400

    un puto comentario uwu

--- Cliente/Cliente.java
+++ Cliente/Cliente.java
@@ -1,44 +1,41 @@
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Scanner;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
 
 public class Cliente {
 
     public static void main(String[] args) {
 
         //Host del servidor
-        final String HOST;    //"127.0.0.1"
-        System.out.println("Ingrese IP para la conexion");
-        
-        Scanner scn = new Scanner(System.in); //despues se rehusa para el menu
+        final String HOST; //"127.0.0.1"
+        System.out.println("Ingrese el puerto para la conexion");
+        Scanner scn = new Scanner(System.in);
         HOST = scn.nextLine();
-
 
         //Puerto del servidor
         final int PUERTO = 11111;
 
         try {
-			
-			
-			
-            //Creo el socket para conectar con cliente
-            Socket sc = new Socket(HOST, PUERTO); //(1) peticion conexión
+            //Creo el socket para conectarme con el cliente
+            Socket sc = new Socket(HOST, PUERTO); //(1)peticion conexión
                         
-                        
-            String mensaje;  //respuesta de menu, se envia a Server
-            
-            
-            //variables ocupadas en ls
-            String ls;  
+
+            boolean flag = true;
+            String mensaje;
+            String ls;
             Boolean lsss;
-            
-            //variables usadas para put y get
-            String palabra;
-            String[] palabras; 
-            
-			boolean flag = true;
+
             while(flag){
 
                 //Menu
@@ -53,12 +50,8 @@ public class Cliente {
                 System.out.println("-----------------------------------");
                 System.out.println("+Ingrese opcion:");
                 System.out.println(" ");
-                
-                
                 mensaje = scn.nextLine();
-                
                 DataOutputStream out = new DataOutputStream(sc.getOutputStream());
-                
                 out.writeUTF(mensaje); //le manda el mensaje al servidor por el socket sc ----> (3) ida al server
 
 
@@ -67,11 +60,10 @@ public class Cliente {
                 }
 
                 if (mensaje.equals("ls")){
-					
                     DataInputStream in = new DataInputStream(sc.getInputStream());
                     lsss = true;
                     while(lsss){
-                        ls = in.readUTF();  //(-><-) lee cada path enviado por el server hasta un mensaje Finisher
+                        ls = in.readUTF();  //(-><-) lee cada path enviado por el server
                         
                         if (ls.equals("Finisher")){
                             lsss=false;
@@ -80,14 +72,13 @@ public class Cliente {
                            System.out.println(ls); 
                         }
                     }
-                    
-                    mensaje = in.readUTF(); //despues borrar esto **
+                    mensaje = in.readUTF();
                 }
 
                 if (mensaje.split(" ")[0].equals("get")) {
-                    
-                    palabras = mensaje.split(" "); 
-                    palabra = palabras[1].replaceAll("\\s",""); 
+                    //InputStream in = sc.getInputStream();
+                    String[] palabras = mensaje.split(" ");
+                    String palabra = palabras[1].replaceAll("\\s",""); //ble.txt por ejemplo
                     try {
                         new Cliente().recivir(sc, palabra);
                     } catch (Exception ex) {
@@ -98,7 +89,7 @@ public class Cliente {
                 if (mensaje.split(" ")[0].equals("put")) {
                     DataInputStream in = new DataInputStream(sc.getInputStream());
 
-                    palabra = in.readUTF(); //lee la palabra que le manda el if de put en server
+                    String palabra = in.readUTF(); //lee la palabra que le manda el if de put en server
                     //OutputStream outs = sc.getOutputStream();
 
                     try {
@@ -110,8 +101,6 @@ public class Cliente {
 
 
                 else if (mensaje.split(" ")[0].equals("delete")){
-					
-					//se envia a Server para que elimine el archivo
                     DataInputStream in = new DataInputStream(sc.getInputStream());
                     System.out.println(in.readUTF());
                 }
@@ -127,45 +116,37 @@ public class Cliente {
     }
 
 
-    public void enviar(String palabra, Socket socket) throws Exception {  //funcion usada por el put
+    public void enviar(String palabra, Socket socket) throws Exception {
 
-        String concatenacion = System.getProperty("user.dir") + "/" + palabra;  //directorio de archivo a enviar
-        
-        //System.out.println(concatenacion);
-        
+        String concatenacion = System.getProperty("user.dir") + "/" + palabra;
+        System.out.println(concatenacion);
         File myFile = new File(concatenacion);
-        int largo = (int) myFile.length(); //largo de archivo
-        String largito = Integer.toString(largo); //same pero en string
+        int largo = (int) myFile.length(); //funciona
+        String largito = Integer.toString(largo);
 
 
         DataOutputStream osss = new DataOutputStream(socket.getOutputStream());
-        
-        osss.writeUTF(largito); //se manda a server el largo del archivo
-        
+        osss.writeUTF(largito);
         DataInputStream issss = new DataInputStream(socket.getInputStream());
         issss.readUTF();
-        
-        
         if(issss.equals("gracias")){
-        	System.out.println("llego el gracias"); //largo recibido por server
+        	System.out.println("llego el gracias");
         }
 
-        byte[] mybytearray = new byte[largo]; //este arreglo contendra el archivo para ser enviado
-        
-		//manejadores de archivos para pasarlo a bytes
-        FileInputStream fis = new FileInputStream(concatenacion);  
+        byte[] mybytearray = new byte[largo];
+        FileInputStream fis = new FileInputStream(concatenacion);
         BufferedInputStream bis = new BufferedInputStream(fis);
-        
-        //se usara para traspaso de bytes via socket
         BufferedOutputStream outt = new BufferedOutputStream(socket.getOutputStream());
 
         int current;
-        current = bis.read(mybytearray); //se pasa al arrego que se mencionó antes
-        
-        //se manda archivo por socket
+        current = bis.read(mybytearray);
         outt.write(mybytearray,0, current);
-        System.out.println("Archivo enviado");
+
+
         
+        //bis.read(mybytearray, 0, mybytearray.length);
+        System.out.println("Archivo enviado");
+        //os.write(mybytearray, 0, mybytearray.length);
         outt.flush();
         fis.close();
         bis.close();
@@ -181,25 +162,20 @@ public class Cliente {
         String nombre = name;
 
         DataInputStream isss = new DataInputStream(socket.getInputStream());
-        largo = isss.readUTF(); //se lee el largo del archivo enviado
-        largito = Integer.parseInt(largo); //se castea a entero
+        largo = isss.readUTF();
+        largito = Integer.parseInt(largo);
 
         DataOutputStream osss = new DataOutputStream(socket.getOutputStream());
-        osss.writeUTF("gracias");  //comunicacion con cliente, listo para la recepción del archivo
+        osss.writeUTF("gracias");
 
-        InputStream iss = socket.getInputStream(); //se crea la conexion con server
-        
-        byte[] mybytearray = new byte[largito]; //arreglo de bytes con largo recibido
-        
-      
-        current = iss.read(mybytearray);  //se traspasa a bytes la informacion recibida
-        
-        //manejadores de archivo nuevo
-        FileOutputStream fos = new FileOutputStream(nombre);  
+        InputStream iss = socket.getInputStream();
+        byte[] mybytearray = new byte[largito];
+        FileOutputStream fos = new FileOutputStream(nombre);
         BufferedOutputStream bos = new BufferedOutputStream(fos);
 
-		//se crea el archivo recibido, desde los bytes
-        bos.write(mybytearray, 0, current); 
+        current = iss.read(mybytearray); //nunca esta leyendo el -1 porqueeee!!!
+
+        bos.write(mybytearray, 0, current);
 
         //bos.write(mybytearray, 0, bytesRead);
         //bos.flush();
