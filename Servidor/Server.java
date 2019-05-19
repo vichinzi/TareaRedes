c08acfc4545ea20dda3ea58722e0dfcc3c70240e
vyagui <vicente.yagui@sansano.usm.cl>
2019-05-19 15:35:52 -0400

    put

--- Servidor/Server.java
+++ Servidor/Server.java
@@ -7,6 +7,7 @@ import java.util.logging.Logger;
 import java.net.URL;
 import java.io.*;
 import java.sql.Timestamp;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Server {
     public static void main(String[] args) {
@@ -155,15 +156,15 @@ public class Server {
 
     }
 
-    public void recivir(Socket socket, String name) throws Exception {
-
-
+    public void recibirparanenvio(Socket socket, String name, String[] ipes, File file, int n_ip, int PUERTO,String nombresinext,String jpg) throws Exception {
 
         String largo;
         int largito;
         int bytesRead;
         int current = 0;
         String nombre = name;
+        int contador = 1;//I like to name parts from 001, 002, 003, ...
+        //you can change it to 0 if you want 000, 001, ...
 
         DataInputStream isss = new DataInputStream(socket.getInputStream());
         largo = isss.readUTF();
@@ -174,19 +175,68 @@ public class Server {
         osss.writeUTF("gracias");
 
         DataInputStream iss = new DataInputStream(socket.getInputStream());
+
         byte[] mybytearray = new byte[largito];
-        FileOutputStream fos = new FileOutputStream(nombre);
-        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        //FileOutputStream fos = new FileOutputStream(nombre);
+        //BufferedOutputStream bos = new BufferedOutputStream(fos);
 
-        iss.readFully(mybytearray); //nunca esta leyendo el -1 porqueeee!!!
+        iss.readFully(mybytearray); //Aqui leo el archivo desde el cliente entrito y lo escribo a mybytearray
 
-        bos.write(mybytearray, 0, largito);
+        //dividir en las maquinas correspondientes
 
-        //bos.write(mybytearray, 0, bytesRead);
-        //bos.flush();
-        System.out.println("Archivo creado");
-        bos.close();
-        fos.close();
+        int largodenvio = 1024 * 64;// 64KB
+        byte[] buffer = new byte[largodenvio];
+
+        //try-with-resources to ensure closing stream
+        int largobase = 0;
+        int nro_index = 0;
+        String str_largo;
+        while (largito > 0) {//mientras queden bytes por enviar
+
+            int randomIPindex = ThreadLocalRandom.current().nextInt(0, n_ip + 1);
+            String ipcita = ipes[randomIPindex];//obtengo la ip donde quedara el pedazito
+            BufferedWriter escribirr = new BufferedWriter(new FileWriter(file,true));//escribo la ip en el index
+            escribirr.write(ipcita);
+            escribirr.close();
+
+            Socket socketmaquina = new Socket(ipcita, PUERTO); //me conecto a la maquina
+            DataOutputStream outps = new DataOutputStream(socketmaquina.getOutputStream());
+            String index_stringeado = Integer.toString(nro_index);
+
+            //IMPORTANTE: FORMATO DE ENVIO ES: Metodo/nombre_archivo_a_crear_en_maquina.txt
+            String nombrearchivo = "put/" + jpg;
+            outps.writeUTF(nombrearchivo); //escribo el nombre que tendra el archivo en la maquina (haya lo guardo)
+            DataInputStream inpst = new DataInputStream(socketmaquina.getInputStream());
+            System.out.println(inpst.readUTF()); //confirmacion de que se recivio el nombre
+            str_largo = Integer.toString(largito);
+            outps.writeUTF(str_largo);
+            System.out.println(inpst.readUTF());//confirmacion de que se recivio el largo
+
+            //falta el handshake!!!
+
+            BufferedOutputStream outmaquina = new BufferedOutputStream(socketmaquina.getOutputStream());
+
+            if (largito > largodenvio) {
+                outmaquina.write(mybytearray, largobase, largodenvio);
+            }else{//si es el pedazo sobrante
+                outmaquina.write(mybytearray, largobase, largodenvio-largito);
+            }
+
+            largobase = largobase + largodenvio;
+            largito = largito - largodenvio;
+            nro_index = nro_index + 1;
+            //leo la confirmacion de la maquina
+            DataInputStream inputmaquina = new DataInputStream(socketmaquina.getInputStream());
+            System.out.println(inputmaquina.readUTF());//archivo creado
+            socketmaquina.close();
+            //out.write(buffer, 0, largodenvio);
+            
+        }
+
+        //bos.write(mybytearray, 0, largito);
+        //System.out.println("Archivo creado");
+        //bos.close();
+        //fos.close();
     }
 }
 
@@ -263,8 +313,17 @@ class ThreadSocket extends Thread{
                     }
                 }
 
-                else if(mensaje.split(" ")[0].equals("get")) {
-                    //OutputStream out = sc.getOutputStream();
+                /*cuando llega una peticion get "archivo" se debe:
+                    -recorrer el archivo INDEX para verificar que exista el index de archivo
+                    -ir al index de archivo ("archivo.txt") y recorrerlo
+                    -Por cada linea del index, tomar la maquina e ir a buscar el pedazo de archivo, y sumarlo en una variable
+                    -finalmente devolverlo al cliente
+                */
+
+                else if(mensaje.split(" ")[0].equals("get")) { 
+
+
+
                     String[] palabras = mensaje.split(" ");
                     String palabra = palabras[1].replaceAll("\\s","");
                     System.out.println(palabra);
@@ -275,15 +334,42 @@ class ThreadSocket extends Thread{
                     }
                 }
 
+                /*Cuando llega una peticion put "archivo" se debe:
+                    -Verificar si existe INDEX.txt, sino, crearlo
+                    -crear el archivo "archivo.txt" que tendra donde se encuentran los pedazos
+                    -dividir cada pedazo con random, escribirlo en "archivo.txt" y enviarlo a su respectiva maquina
+                    -luego de enviar todos los pedazos, escribir en el INDEX que el archivo tiene su index llamado "archivo.txt"
+
+                */
+
                 else if(mensaje.split(" ")[0].equals("put")) {
 
+                    //Recivimos put "archivo" y obtenemos la palabra
                     String[] palabras = mensaje.split(" ");
-                    String palabra = palabras[1].replaceAll("\\s","");
+                    String palabra = palabras[1].replaceAll("\\s",""); //algo.jpg
+
+                    String[] sinexts = palabra.split(".");
+                    String nombre = sinexts[0] + ".txt";
+
+                    //Creamos el index palabra.txt
+                    File archivo = new File(nombre);
+                    if (archivo.createNewFile()) {  
+                        System.out.println("Se creo el archivo" + nombre);
+                    }
+
+ 
+                    //Redundancia necesaria
                     DataOutputStream out = new DataOutputStream(sc.getOutputStream());
                     out.writeUTF(palabra);
-                    //InputStream ins = sc.getInputStream();
+                    
                     try {
-                        new Server().recivir(sc,palabra);
+                        new Server().recibirparanenvio(sc,nombre,ips,archivo,n_ip,11111,sinexts[0],palabra);
+
+                        String adesong = palabra + " " + nombre;
+                        BufferedWriter indx = new BufferedWriter(new FileWriter(file,true));//escribo la ip en el index
+                        indx.write(adesong);
+                        indx.close();
+
                     } catch (Exception ex) {
                         Logger.getLogger(ThreadSocket.class.getName()).log(Level.SEVERE, null, ex);
                     }
